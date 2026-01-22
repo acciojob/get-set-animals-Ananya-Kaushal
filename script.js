e0edb615bf8706ece66db1b0d91493fd29555d59
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2026-01-22 23:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species) {
+    // Store species in a property prefixed with an underscore
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+	bark() {
+    console.log("woof");
+}
+
+class Cat extends Animal {
+	purr() {
+    console.log("purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
